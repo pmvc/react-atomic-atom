d1ca42dbf1336f8ce83a5ddb5ae1dc80e2bc11ce
Hill <hill@kimo.com>
2015-10-11 00:24:13 +0400

    change to es6 style

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-module.exports = {
-    div: require('../ui/atoms/div'),
-    span: require('../ui/atoms/span'),
-    li: require('../ui/atoms/li'),
-    h1: require('../ui/atoms/h1'),
-    h2: require('../ui/atoms/h2'),
-    h3: require('../ui/atoms/h3'),
-    h4: require('../ui/atoms/h4'),
-    h5: require('../ui/atoms/h5'),
-    h6: require('../ui/atoms/h6'),
-    ol: require('../ui/atoms/ol'),
-    ul: require('../ui/atoms/ul'),
-    nav: require('../ui/atoms/nav'),
-    button: require('../ui/atoms/button'),
-    form: require('../ui/atoms/form'),
-    input: require('../ui/atoms/input'),
-    img: require('../ui/atoms/img'),
-    a: require('../ui/atoms/a'),
-    svg: require('../ui/atoms/svg'),
-    path: require('../ui/atoms/path'),
-    i: require('../ui/atoms/i'),
-}
+export { default as A } from '../ui/atoms/a';
+export { default as Button } from '../ui/atoms/button';
+export { default as Div } from '../ui/atoms/div';
+export { default as Form } from '../ui/atoms/form';
+export { default as H1 } from '../ui/atoms/h1';
+export { default as H2 } from '../ui/atoms/h2';
+export { default as H3 } from '../ui/atoms/h3';
+export { default as H4 } from '../ui/atoms/h4';
+export { default as H5 } from '../ui/atoms/h5';
+export { default as H6 } from '../ui/atoms/h6';
+export { default as I } from '../ui/atoms/i';
+export { default as Img } from '../ui/atoms/img';
+export { default as Input } from '../ui/atoms/input';
+export { default as Li } from '../ui/atoms/li';
+export { default as Nav } from '../ui/atoms/nav';
+export { default as Ol } from '../ui/atoms/ol';
+export { default as Path } from '../ui/atoms/path';
+export { default as Span } from '../ui/atoms/span';
+export { default as Svg } from '../ui/atoms/svg';
+export { default as Ul } from '../ui/atoms/ul';
+
