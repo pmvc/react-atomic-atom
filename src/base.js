b0c438b37ae99959f46c33d515cf4ca67145998e
Hill <hill@kimo.com>
2017-06-14 06:06:27 +0000

    improve base

--- src/base.js
+++ src/base.js
@@ -3,8 +3,7 @@
 import React from 'react';
 
 const Base = (name) => {
-    const Atom = (props) => {
-        let {refCb, ...others} = props;
+    const Atom = ({refCb, ...others}) => {
         if (refCb) {
             others.ref = refCb;
         }
