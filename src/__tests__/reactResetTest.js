c01ec8354767622609b38dcbeda3866ac318b246
Hill <hill@kimo.com>
2022-04-08 03:48:35 +0800

    Test best react rest

--- src/__tests__/reactResetTest.js
+++ src/__tests__/reactResetTest.js
@@ -5,11 +5,19 @@ import {
   waitFor,
   hideConsoleError,
   getSinon as sinon,
+  cleanIt,
 } from "reshow-unit";
 
 import base from "../base";
 
-describe("Test base", () => {
+/**
+ * undefined could get better performance compare with delete.
+ * https://github.com/react-atomic/reshow/issues/117
+ */
+
+describe("Test best react rest", () => {
+  afterEach(() => cleanIt());
+
   it("test with null", () => {
     const Dom = base("div");
     const wrap = render(<Dom data-foo="bar" data-bar={null} />);
@@ -26,5 +34,31 @@ describe("Test base", () => {
     const wrap = render(<Dom data-foo="bar" AAA={undefined} />);
     expect(wrap.html()).to.equal(`<div data-foo="bar"></div>`);
     expect(spy.called).to.be.true;
+  });
+
+  it("test object merge with undefined", async () => {
+    /**
+     * Object merge will not trigger error
+     */
+    const o = {
+      "data-foo": "bar",
+      AAA: undefined,
+    };
+    const Dom = base("div");
+    const wrap = render(<Dom {...o} />);
+    expect(wrap.html()).to.equal(`<div data-foo="bar"></div>`);
+  });
+
+  it("test object merge replace for undefined", () => {
+    const o = {
+      "data-foo": "bar",
+      AAA: "BBB",
+    };
+    expect(o.AAA).to.equal("BBB");
+    const o1 = {
+      ...o,
+      AAA: undefined,
+    };
+    expect(o1.AAA).to.be.undefined;
   });
 });
