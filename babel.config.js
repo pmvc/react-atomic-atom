fb2c9dd626ad7d760bd64a8b981bbcaa298d7329
Hill <hill@kimo.com>
2022-04-15 20:51:03 +0800

    move babel to base config

--- babel.config.js
+++ babel.config.js
@@ -15,19 +15,22 @@ module.exports = function (api) {
         plugins: [
           "add-module-exports",
           "dynamic-import-node",
+          "reshow-object-to-json-parse",
           "transform-react-pure-class-to-function",
           ["transform-react-remove-prop-types", { mode: "wrap" }],
           [
             "reshow-transform-runtime",
             {
+              // https://babel.dev/docs/en/babel-plugin-transform-runtime#regenerator
               regenerator: true,
+              // https://github.com/react-atomic/reshow/tree/main/packages/reshow-app
               version: "7.17.0",
             },
           ],
           "@babel/plugin-proposal-export-default-from",
           "@babel/plugin-syntax-dynamic-import",
-          "@babel/plugin-transform-react-constant-elements",
           "@babel/plugin-transform-object-assign",
+          "@babel/plugin-transform-react-constant-elements",
           "@babel/plugin-proposal-object-rest-spread",
           "@babel/plugin-proposal-class-properties",
         ],
@@ -44,13 +47,17 @@ module.exports = function (api) {
           ["@babel/preset-react", { runtime: "automatic" }],
         ],
         plugins: [
+          "reshow-object-to-json-parse",
           "transform-react-pure-class-to-function",
+          ["add-import-extension", { extension: "mjs", replace: true }],
           ["transform-react-remove-prop-types", { mode: "wrap" }],
           [
             "reshow-transform-runtime",
             {
+              // https://babel.dev/docs/en/babel-plugin-transform-runtime#regenerator
               regenerator: true,
               useESModules: true,
+              // https://github.com/react-atomic/reshow/tree/main/packages/reshow-app
               version: "7.17.0",
             },
           ],
