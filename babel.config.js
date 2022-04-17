05a6b25146618780ca1eb7c7c2ce6351cd30c8cc
Hill <hill@kimo.com>
2022-04-17 20:54:09 +0800

    improve babel config

--- babel.config.js
+++ babel.config.js
@@ -1,96 +1,82 @@
-module.exports = function (api) {
+module.exports = function (api, { esm = true } = {}) {
   api.cache(true);
-  return {
-    env: {
-      cjs: {
-        presets: [
-          [
-            "@babel/preset-env",
-            {
-              corejs: 3,
-              useBuiltIns: "usage",
-              loose: true,
-              targets: ["last 2 versions", "ie >= 8"],
-            },
-          ],
-          ["@babel/preset-react", { runtime: "automatic" }],
-        ],
-        plugins: [
-          "add-module-exports",
-          "dynamic-import-node",
-          "reshow-object-to-json-parse",
-          "transform-react-pure-class-to-function",
-          ["transform-react-remove-prop-types", { mode: "wrap" }],
-          [
-            "reshow-transform-runtime",
-            {
-              // https://babel.dev/docs/en/babel-plugin-transform-runtime#regenerator
-              regenerator: true,
-              // https://github.com/react-atomic/reshow/tree/main/packages/reshow-app
-              version: "7.17.0",
-            },
-          ],
-          "@babel/plugin-syntax-dynamic-import",
-          "@babel/plugin-transform-arrow-functions",
-          "@babel/plugin-transform-object-assign",
-          "@babel/plugin-transform-react-constant-elements",
-          "@babel/plugin-transform-spread",
-          "@babel/plugin-proposal-class-properties",
-          "@babel/plugin-proposal-export-default-from",
-          "@babel/plugin-proposal-nullish-coalescing-operator",
-          "@babel/plugin-proposal-optional-chaining",
-          "@babel/plugin-proposal-object-rest-spread",
-          ["@babel/plugin-proposal-private-methods", { loose: false }],
-          [
-            "@babel/plugin-proposal-private-property-in-object",
-            { loose: false },
-          ],
-        ],
+  const samePlugIns = [
+    "reshow-object-to-json-parse",
+    "transform-react-pure-class-to-function",
+    ["transform-react-remove-prop-types", { mode: "wrap" }],
+    "@babel/plugin-syntax-dynamic-import",
+    "@babel/plugin-transform-arrow-functions",
+    "@babel/plugin-transform-object-assign",
+    "@babel/plugin-transform-react-constant-elements",
+    "@babel/plugin-transform-spread",
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-export-default-from",
+    "@babel/plugin-proposal-nullish-coalescing-operator",
+    "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-object-rest-spread",
+    ["@babel/plugin-proposal-private-methods", { loose: false }],
+    ["@babel/plugin-proposal-private-property-in-object", { loose: false }],
+  ];
+  const esPlugIns = [
+    ...samePlugIns,
+    [
+      "reshow-transform-runtime",
+      {
+        // https://babel.dev/docs/en/babel-plugin-transform-runtime#regenerator
+        regenerator: true,
+        useESModules: true,
+        // https://github.com/react-atomic/reshow/tree/main/packages/reshow-app
+        version: "7.17.0",
       },
-      es: {
-        presets: [
-          [
-            "@babel/preset-env",
-            {
-              modules: false,
-              loose: true,
-              targets: ["last 2 versions", "ie >= 8"],
-            },
-          ],
-          ["@babel/preset-react", { runtime: "automatic" }],
-        ],
-        plugins: [
-          "reshow-object-to-json-parse",
-          "transform-react-pure-class-to-function",
-          ["add-import-extension", { extension: "mjs", replace: true }],
-          ["transform-react-remove-prop-types", { mode: "wrap" }],
-          [
-            "reshow-transform-runtime",
-            {
-              // https://babel.dev/docs/en/babel-plugin-transform-runtime#regenerator
-              regenerator: true,
-              useESModules: true,
-              // https://github.com/react-atomic/reshow/tree/main/packages/reshow-app
-              version: "7.17.0",
-            },
-          ],
-          "@babel/plugin-syntax-dynamic-import",
-          "@babel/plugin-transform-arrow-functions",
-          "@babel/plugin-transform-object-assign",
-          "@babel/plugin-transform-react-constant-elements",
-          "@babel/plugin-transform-spread",
-          "@babel/plugin-proposal-class-properties",
-          "@babel/plugin-proposal-export-default-from",
-          "@babel/plugin-proposal-nullish-coalescing-operator",
-          "@babel/plugin-proposal-optional-chaining",
-          "@babel/plugin-proposal-object-rest-spread",
-          ["@babel/plugin-proposal-private-methods", { loose: false }],
-          [
-            "@babel/plugin-proposal-private-property-in-object",
-            { loose: false },
-          ],
-        ],
-      },
-    },
+    ],
+  ];
+  if (esm) {
+    esPlugIns.push([
+      "add-import-extension",
+      { extension: "mjs", replace: true },
+    ]);
+  }
+  const cjs = {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          corejs: 3,
+          useBuiltIns: "usage",
+          loose: true,
+          targets: ["last 2 versions", "ie >= 8"],
+        },
+      ],
+      ["@babel/preset-react", { runtime: "automatic" }],
+    ],
+    plugins: [
+      ...samePlugIns,
+      "add-module-exports",
+      "dynamic-import-node",
+      [
+        "reshow-transform-runtime",
+        {
+          // https://babel.dev/docs/en/babel-plugin-transform-runtime#regenerator
+          regenerator: true,
+          // https://github.com/react-atomic/reshow/tree/main/packages/reshow-app
+          version: "7.17.0",
+        },
+      ],
+    ],
   };
+  const es = {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          modules: false,
+          loose: true,
+          targets: ["last 2 versions", "ie >= 8"],
+        },
+      ],
+      ["@babel/preset-react", { runtime: "automatic" }],
+    ],
+    plugins: esPlugIns,
+  };
+  return { env: { cjs, es } };
 };
